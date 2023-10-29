f6a136169c08dc75c151031c3cb7012ecd99601a
NikolaiGushchin <ni.gushchin@gmail.com>
2023-10-29 16:40:47 +0600

    feat: slider for last project

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".last-project__pagination",
+    bulletClass: "last-project__bullet",
+    bulletActiveClass: "last-project__bullet_active",
+    clickable: true
+  },
+});
